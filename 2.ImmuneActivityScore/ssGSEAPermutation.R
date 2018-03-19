ec1b4d213acefc0ff24d4e5d93276d4c36813a8b
Elena <35086672+dengchunyu@users.noreply.github.com>
2018-03-19 16:41:03 +0800

    Update ssGSEAPermutation.R

--- 2.ImmuneActivityScore/ssGSEAPermutation.R
+++ 2.ImmuneActivityScore/ssGSEAPermutation.R
@@ -1,3 +1,11 @@
+
+#' @@ ssGSEA.MultipleSamples.Permutation
+#' @description  Perturb the rownames of an expression profile in order to obtain a random expression profile.
+#'
+#' @param  exp.profile A data.frame indicating the real expression profile.
+#' @returnType data.frame
+#' @return data.frame of random expression profile
+
 ssGSEA.MultipleSamples.Permutation <- function(exp.profile){
   permutation.geneTags <- sample(rownames(exp.profile), size = nrow(exp.profile), replace = FALSE)
   permutation.profile <- exp.profile[permutation.geneTags, ]
