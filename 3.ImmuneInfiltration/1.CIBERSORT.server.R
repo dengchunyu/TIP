17cc5e0bc6b5a6c8f6ce2bcbb2c2c7e6becf92f2
Elena <35086672+dengchunyu@users.noreply.github.com>
2018-03-19 14:49:31 +0800

    Update 1.CIBERSORT.server.R

--- 3.ImmuneInfiltration/1.CIBERSORT.server.R
+++ 3.ImmuneInfiltration/1.CIBERSORT.server.R
@@ -1,5 +1,14 @@
-#' Main functions
-#' Author: DengChunyu, Haerbin Medical University, dengcyelena@gmail.com
+#' @title
+#' Immune cell infiltration proportion calculate module  
+#'  
+#' @aliases CIBERSORT  
+#'   
+#' @description
+#' A reference function to produce the calculate cell infiltration proportions results 
+#' and interactive picture data of the TIP website. Inference proceeds via four functions : 
+#' CIBERSORT_main.R\CIBERSORT_func.R\CoreAlg.R\doPerm.R. 
+#' 
+#' @author DengChunyu, Haerbin Medical University, dengcyelena@gmail.com
 #' Requirements:
 #'       R v3.0 or later. (dependencies below might not work properly with earlier versions)
 #'       install.packages('e1071')
@@ -9,10 +18,19 @@
 #'           source("http://bioconductor.org/biocLite.R")
 #'           biocLite("preprocessCore")
 #' Windows users using the R GUI may need to Run as Administrator to install or update packages.
-#' This script import 5 R files.
+#'
+#' @details 
+#' This function is run after 'immunityScore.server.R'(look at 'test.R'), so the input data is come
+#' from one of the result of 'immunityScore.server.R', the name of input data is definitized in the 
+#' code:'epression.from.users.tpm.RData'.
 #' 
+#'
+#' @example 
 #'   Example In R:
 #'       source('CIBERSORT.server.R')
+#'       library('e1071')
+#'       library('parallel')
+#'       library('preprocessCore')
 #'       
 #'       CIBERSORT.server(codePath="",
 #'       filePath="",
@@ -22,18 +40,35 @@
 #'       CHIPorRNASEQ="Microarray",
 #'       sample="multiple");
 #' 
-#' @param codePath: the storage path of import code files.
-#' @param filePath ：the storage path of the users data.
-#' @param fileName ：the name of the users file.this parameter is useless now after optimization.
-#' @param signaturePath: the storage path of signature matrix files.
-#' @param saveDir： the storage path of results.
-#' @param perm： the NO. permutation.default=100
-#' @param CHIPorRNASEQ: the type of data,RNAseq or Microarray.
-#' @param sample： "multiple" or "single" sample for upload files.
-#' @param dataType: "count" or "TPM" for upoad data.
-#' library('e1071')
-#' library('parallel')
-#' library('preprocessCore')
+#' 
+#' @param codePath:
+#' The storage path of import code files, including four files.
+#' 
+#' @param filePath ：the storage path of the users data, and you must make sure 
+#' 'epression.from.users.tpm.RData' is in the path.
+#' 
+#' @param fileName ：
+#' the name of the users file. when you run 'test.R', this parameter is useless, but we didn't give up
+#' it in case when you want to run 'CIBERSORT.server.R' independently.
+#' 
+#' @param signaturePath: 
+#' the storage path of signature matrix files.
+#' 
+#' @param saveDir： 
+#' the storage path of results.
+#' 
+#' @param perm：
+#' The NO. permutation.default by 100
+#' 
+#' @param CHIPorRNASEQ:
+#' The source of data,'RNAseq' or 'Microarray' to choose.default by "RNA-seq"
+#' 
+#' @param sample： 
+#' "multiple" or "single" sample for upload files to choose.default by "multiple"
+#' 
+#' @param dataType: 
+#' "count" or "TPM" for upoad data type to choose.default by "TPM"
+#' 
 #' @export 
 
 CIBERSORT.server <- function(codePath, filePath, fileName= "",signaturePath, saveDir, perm = 100,CHIPorRNASEQ="RNA-seq", sample="multiple",dataType="TPM"){
@@ -44,7 +79,7 @@ CIBERSORT.server <- function(codePath, filePath, fileName= "",signaturePath, sav
   source(sprintf("%s/3.CIBERSORT_func.R",codePath));
   source(sprintf("%s/4.CoreAlg.R",codePath));
   source(sprintf("%s/5.doPerm.R",codePath));
-  source(sprintf("%s/6.Count2TPM3.0.R",codePath));
+  #source(sprintf("%s/6.Count2TPM3.0.R",codePath));
   
   print("The Infiltration source codes is import!");
 
