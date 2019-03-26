11d5c593bdd1f5ab6f83ab0a9448b40a6160a4f0
Elena <35086672+dengchunyu@users.noreply.github.com>
2019-03-26 20:46:33 +0800

    Update PCAScatterPlot.R

--- 2.ImmuneActivityScore/PCAScatterPlot.R
+++ 2.ImmuneActivityScore/PCAScatterPlot.R
@@ -4,3 +4,14 @@ PCASatterPlot <- function(pca.matrix, save.file){
   total.spots <- paste0("[", paste(each.spot, collapse = ","), "]")
   write(total.spots, file = save.file)
 }
+
+
+
+BarPlotFormat <- function(score.matrix, save.dir){
+  #7 step scores of one sample save into a file
+  for(col.num in 1:ncol(score.matrix)){
+    separate.step <- paste0('{"name":"',rownames(score.matrix),'","value":', score.matrix[, col.num], "}")
+    onesample <- paste0("[", paste(separate.step, collapse = ","), "]")
+    write(onesample, file = paste0(save.dir,"/",colnames(score.matrix)[col.num], "_BarPlotFormat.txt"))
+  }
+}
