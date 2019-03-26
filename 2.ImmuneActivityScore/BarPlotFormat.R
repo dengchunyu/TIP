7f1a7778992ff690e9bffb057b793b022d2bc230
Elena <35086672+dengchunyu@users.noreply.github.com>
2019-03-26 21:04:50 +0800

    Delete BarPlotFormat.R

--- 2.ImmuneActivityScore/BarPlotFormat.R
+++
@@ -1,8 +0,0 @@
-BarPlotFormat <- function(score.matrix, save.dir){
-  #7 step scores of one sample save into a file
-  for(col.num in 1:ncol(score.matrix)){
-    separate.step <- paste0('{"name":"',rownames(score.matrix),'","value":', score.matrix[, col.num], "}")
-    onesample <- paste0("[", paste(separate.step, collapse = ","), "]")
-    write(onesample, file = paste0(save.dir,"/",colnames(score.matrix)[col.num], "_BarPlotFormat.txt"))
-  }
-}
